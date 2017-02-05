7ae5adb346c39cc8c5610cd9ed7b7d9cd3128372
Aniello Cristo <aniello.cristo@gmail.com>
2017-02-04 23:43:31 -0600

    added wiki API

--- javascript/script.js
+++ javascript/script.js
@@ -12,6 +12,47 @@ $(document).ready(function(){
   //Search Button Fold Effect
 
   $(".img1").click(function() {
-  $(".fade2").toggle( "fold" );
-});
+  $(this).fadeOut().hide();
+  $(".img2").fadeIn().show();
+  $(".fade2").toggle("fold").focus();
+  });
+
+  $(".img2").click(function() {
+    $(".fade2").toggle( "fold" );
+    $("#user-input").val('');
+    $(this).fadeOut().hide();
+    $(".img1").fadeIn().show();
+  });
+
+//Wikipedia API Call
+
+  $("#user-input").keypress(function(event){
+    var keycode = event.which;
+      if(keycode == '13'){
+
+      var search = $("#user-input").val();
+      var source = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&generator=search&exchars=600&exlimit=10&exintro=1&gsrsearch="+search+"&gsrlimit=10";
+
+      $.ajax({
+        type: "GET",
+        url: source,
+        dataType: "jsonp",
+        crossDomain: true,
+        success: function(data){
+          display(data);
+        },
+        error: function(error) {
+          console.log("error");
+        }
+      });
+    };
+  });
+
+function display(data) {
+  var pages = data.query.pages
+  for(object in pages) {
+      $(".results").append('<div class="item"><h6>'+pages[object].title+'</h6><p class="extract">'+pages[object].extract+'</p></div>');
+  }
+}
+
 });
