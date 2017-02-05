39ed75870ff911a09791f139bc10f674847be937
Aniello Cristo <aniello.cristo@gmail.com>
2017-02-05 15:24:45 -0600

    added clearing effects

--- javascript/script.js
+++ javascript/script.js
@@ -15,13 +15,22 @@ $(document).ready(function(){
   $(this).fadeOut().hide();
   $(".img2").fadeIn().show();
   $(".fade2").toggle("fold").focus();
+  $("#user-input").keydown(function(){
+    $(".results").empty();
+  });
   });
 
   $(".img2").click(function() {
     $(".fade2").toggle( "fold" );
     $("#user-input").val('');
+    $(".results").empty();
     $(this).fadeOut().hide();
     $(".img1").fadeIn().show();
+  });
+
+  //Result hover effect
+  $(".item").mouseover(function(){
+    $(this).css("background-color", "rgb(220, 210, 245)");
   });
 
 //Wikipedia API Call
@@ -29,6 +38,8 @@ $(document).ready(function(){
   $("#user-input").keypress(function(event){
     var keycode = event.which;
       if(keycode == '13'){
+
+
 
       var search = $("#user-input").val();
       var source = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&generator=search&exchars=600&exlimit=10&exintro=1&gsrsearch="+search+"&gsrlimit=10";
@@ -48,10 +59,15 @@ $(document).ready(function(){
     };
   });
 
+// Append api informatino to HTML elements
 function display(data) {
   var pages = data.query.pages
-  for(object in pages) {
-      $(".results").append('<div class="item"><h6>'+pages[object].title+'</h6><p class="extract">'+pages[object].extract+'</p></div>');
+  var link = "https://en.wikipedia.org/?curid=";
+    for(object in pages) {
+      var id = pages[object].pageid
+
+      $(".results").append(`<a href="${link+id}"><div class="item"><h6>${pages[object].title}</h6><p class="extract">${pages[object].extract}</p></div></a>`);
+
   }
 }
 
